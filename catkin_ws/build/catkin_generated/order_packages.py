d6c7c429bf0ae310054404eef8f4d76b176c7b45
Markus Simonsen <markus.k.simonsen@gmail.com>
2024-03-19 19:45:30 +0100

    lgtm

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/markus/ros_stuff/ba_mobile_manipulator/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
+whitelisted_packages = 'our_amazing_code'.split(';') if 'our_amazing_code' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/markus/ros_stuff/ba_mobile_manipulator/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/markus/ros_stuff/ba_mobile_manipulator/catkin_ws/devel;/opt/ros/noetic' != '' else []
